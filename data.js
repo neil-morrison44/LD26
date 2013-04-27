582542ed21b04354b6a119f2cbf8b05d6da53c48
Neil Morrison <neil@Neil-Morrisons-MacBook-Pro.local>
2013-04-27 16:15:23 +0100

    progress

---
+++ data.js
@@ -0,0 +1,11 @@
+var data={"buzzwords":[],"actors":[
+{"name":"none","nips":[{"x":0,"y":0}],
+"pants":{"x":0,"y":0,"width":10},
+"hat":{"x":0,"y":0,"width":20}}
+],
+"nips":[{"imagesrc":null,"image":null,"name":"none"}],
+"hat":[{"imagesrc":null,"image":null,"name":"none"}],
+
+"sentences":["find sexy"]
+
+}
