3fb9604e2b6778f138f3a578bf576f7068eff777
Neil Morrison <neil@Neil-Morrisons-MacBook-Pro.local>
2013-04-27 22:51:28 +0100

    last push for sat night

--- data.js
+++ data.js
@@ -1,4 +1,21 @@
-var data={"buzzwords":[],"actors":[
+var data={"buzzwords":{'start':['user','second-screen','global','ergonomic',
+		'blog-o-sphere','cloud','deep','big data','forward thinking','digital','open source',
+		'Facebook','Google+','Kickstarted',
+		'instant','always-on','interactive','Netflix','meme','meta -','micro','new media','tag','twitter','virtual',
+		'hashtag','molyneuxian','minecraft'],
+		
+	'middle':['centric','driven','enhanced','augmented','data driven',
+			  'sharing','reactive','digital','motivated','web 3.0','balls to the walls',
+			  'geolocating','kickstarting','grid','interaction','feed based',
+			  'social','crowd','think-tank','search','live','creative'],
+			  
+	'end':['design','experience','tweet','blog','cloud','media source','Kickstarter',
+	       'visualisation','app','workplace','web app','mobile app','computation','mash-up',
+	       'homebrew','portfolio','network','wiki','engine','consumer','media center','center','console', 'start-up',
+	       'market']
+}
+,
+"actors":[
 {"name":"none","nips":[{"x":0,"y":0}],
 "pants":{"x":0,"y":0,"width":10},
 "hat":{"x":0,"y":0,"width":20}}
