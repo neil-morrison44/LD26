4a26043c6ca1ad4f9c6632f86ddf57f270f5f5fa
Neil Morrison <neil@Neil-Morrisons-MacBook-Pro.local>
2013-04-29 00:59:46 +0100

    possibly final commit

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var data={"buzzwords":{'start':['user','second-screen','global','ergonomic',
 		'blog-o-sphere','cloud','deep','big data','forward thinking','digital','open source',
 		'Facebook','Google+','Kickstarted',
 		'instant','always-on','interactive','Netflix','meme','meta -','micro','new media','tag','twitter','virtual',
-		'hashtag','molyneuxian','minecraft'],
+		'hashtag','molyneuxian','minecraft','indie'],
 		
 	'middle':['centric','driven','enhanced','augmented','data driven',
 			  'sharing','reactive','digital','motivated','web 3.0','balls to the walls',
@@ -19,19 +19,30 @@ var data={"buzzwords":{'start':['user','second-screen','global','ergonomic',
 {"name":"crocodiles","nips":[{"x":60,"y":131},{"x":101,"y":132}],
 "pant":{"x":42,"y":216,"width":75},
 "hat":{"x":52,"y":38,"width":68},"imagesrc":"croc.png","image":null},
+
 {"name":"octopi","nips":[{"x":69,"y":27},{"x":20,"y":49},{"x":54,"y":68},{"x":10,"y":125},{"x":245,"y":62},{"x":272,"y":106},{"x":243,"y":163}],
 "pant":{"x":90,"y":173,"width":85},
-"hat":{"x":78,"y":38,"width":160},"imagesrc":"octopus.png","image":null}
+"hat":{"x":78,"y":38,"width":160},"imagesrc":"octopus.png","image":null},
+
+{"name":"cows","nips":[{"x":71,"y":235},{"x":87,"y":238},{"x":75,"y":244},{"x":82,"y":244}],
+"pant":{"x":43,"y":237,"width":93},
+"hat":{"x":73,"y":26,"width":61},"imagesrc":"cow.png","image":null},
 ],
 
-"nips":[{"imagesrc":"goldTassel.png","image":null,"tags":['sexy']}],
+"nips":[{"imagesrc":"goldTassel.png","image":null,"tags":['sexy']},
+{"imagesrc":"tardis.png","image":null,"tags":['time']},
+{"imagesrc":"nip-spike.png","image":null,"tags":['danger']}],
+
 "hats":[{"imagesrc":"bowler.png","image":null,"tags":['classy']},
 {"imagesrc":"fez.png","image":null,"tags":["exotic","time"]}],
-"pants":[{"imagesrc":"heart.png","image":null,"tags":['lovely']}],
+
+"pants":[{"imagesrc":"heart.png","image":null,"tags":['lovely']},
+{"imagesrc":"nappy.png","image":null,"tags":['baby']}],
 
 "sentences":["find {w}, {w}, {a} in your area!","{w} {a} are waiting to chat with you now!","Hi, I'm a {w}, {w} {a}!"],
 "wordlist":[{"words":["lovely"],"tags":["lovely"]},{"words":["time-travelling"],"tags":["time"]},
-			{"words":["exotic"],"tags":["exotic"]},
+			{"words":["exotic"],"tags":["exotic"]},{"words":["dangerous"],"tags":["danger"]},
+			{"words":["young","baby"],"tags":["baby"]},
 			{"words":["sexy"],"tags":["sexy"]},{"words":["classy"],"tags":["classy"]}
 ]
 }
