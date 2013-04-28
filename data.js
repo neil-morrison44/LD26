63386194619f82b022692ba87cfe495587eafa73
Neil Morrison <neil@Neil-Morrisons-MacBook-Pro.local>
2013-04-28 22:23:43 +0100

    sound, octo,~cow

--- data.js
+++ data.js
@@ -16,13 +16,22 @@ var data={"buzzwords":{'start':['user','second-screen','global','ergonomic',
 }
 ,
 "actors":[
-{"name":"none","nips":[{"x":0,"y":0}],
-"pants":{"x":0,"y":0,"width":10},
-"hat":{"x":0,"y":0,"width":20}}
+{"name":"crocodiles","nips":[{"x":60,"y":131},{"x":101,"y":132}],
+"pant":{"x":42,"y":216,"width":75},
+"hat":{"x":52,"y":38,"width":68},"imagesrc":"croc.png","image":null},
+{"name":"octopi","nips":[{"x":69,"y":27},{"x":20,"y":49},{"x":54,"y":68},{"x":10,"y":125},{"x":245,"y":62},{"x":272,"y":106},{"x":243,"y":163}],
+"pant":{"x":90,"y":173,"width":85},
+"hat":{"x":78,"y":38,"width":160},"imagesrc":"octopus.png","image":null}
 ],
-"nips":[{"imagesrc":null,"image":null,"name":"none"}],
-"hat":[{"imagesrc":null,"image":null,"name":"none"}],
 
-"sentences":["find sexy"]
+"nips":[{"imagesrc":"goldTassel.png","image":null,"tags":['sexy']}],
+"hats":[{"imagesrc":"bowler.png","image":null,"tags":['classy']},
+{"imagesrc":"fez.png","image":null,"tags":["exotic","time"]}],
+"pants":[{"imagesrc":"heart.png","image":null,"tags":['lovely']}],
 
+"sentences":["find {w}, {w}, {a} in your area!","{w} {a} are waiting to chat with you now!","Hi, I'm a {w}, {w} {a}!"],
+"wordlist":[{"words":["lovely"],"tags":["lovely"]},{"words":["time-travelling"],"tags":["time"]},
+			{"words":["exotic"],"tags":["exotic"]},
+			{"words":["sexy"],"tags":["sexy"]},{"words":["classy"],"tags":["classy"]}
+]
 }
